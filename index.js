ba55e78cbb61a57d94bfc2b034c05c80e20827f9
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-06-10 19:45:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 a = 5
 b = 5
-n = 4
+
 function add(a, b){
 return a + b
 }
@@ -14,5 +14,8 @@ function divide(a, b){
 return a / b
 }
 function increment(n){
-return n--
+return n++
+}
+function decrement(n){
+  return n--
 }
