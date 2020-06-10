f7cd930e795e72c6213f3e65ec0a12e67cd77f49
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-06-10 19:30:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+a = 5
+b = 5
+n = 8
+function add(a, b){
+return a + b
+}
+function subtract(a, b){
+return a - b
+}
+function multiply(a, b){
+return a * b
+}
+function divide(a, b){
+return a / b
+}
+function increment(n){
+  return n++
+}
+function decrement(n){
+  return n--
+}
+function makeInt(n){
+  n = '10'
+  parseInt(n)
+  return n
+}
