c148c81d5ac91c764bbc4248cb73219ed9bdf13c
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-06-10 19:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 function add(a, b){
-return a + b
+  return a + b
 }
 function subtract(a, b){
-return a - b
+  return a - b
 }
 function multiply(a, b){
-return a * b
+  return a * b
 }
 function divide(a, b){
-return a / b
+  return a / b
 }
-function increment(a){
-return (a++)
+function increment(n){
+  return (n++)
 }
-function decrement(a){
+function decrement(n){
+  return (n--)
 }
-decrement()
-return (a--)
