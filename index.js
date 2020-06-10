d0e88317f4b572bb17864ea3caf3f16b69b2e542
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-06-10 20:11:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-
-function add(a, b){
-number = a + b;
-return number;
-}
-function subtract(a, b){
-number = a - b;
-return number;
-}
-function multiply(a, b){
-number = a * b;
-return number;
-}
-function divide(a, b){
-number = a / b;
-return number;
-}
-function increment(n){
-  number = n++;
+var number;
+function add(a, b){ //add a and b and return the result.
+  number = a + b;
   return number;
 }
-function decrement(n){
-  number = n--;
+function subtract(a, b){ //subtract a and b and return the result.
+  number = a - b;
   return number;
 }
-function makeInt(n){
-  number = parseInt(n, 10);
+function multiply(a, b){ //multiply a and b and return the result.
+  number = a * b;
   return number;
 }
-function perserveDecimal(n){
-  number = parseFloat(n, 12);
-  return number
+function divide(a, b){ //divide a and b and return the result.
+  number = a / b;
+  return number;
+}
+function inc(a){ //Increment a by 1 and return the result.
+  number = a + 1;
+  return number;
+}
+function dec(a){ //Decrement a by 1 and return the result.
+  number = a - 1;
+  return number;
+}
+function makeInt(a){ //Parse a as an integer and return that integer.
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ //Parse a as a float (number with decimals) and return that number.
+  number = parseFloat(a, 10);
+  return number;
 }
